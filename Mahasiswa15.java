ce6d0038f088ae90860522150ef31f009ec072c8
maullidya <zhramldy@gmail.com>
2026-02-20 10:21:19 +0700

    percobaan 3

--- Mahasiswa15.java
+++ Mahasiswa15.java
@@ -32,4 +32,11 @@ public class Mahasiswa15 {
             return "Kinerja kurang";
         }
     }
+    public Mahasiswa15(){}
+    public Mahasiswa15(String nm, String nim, double ipk, String kls){
+        nama = nm;
+        this.ipk = ipk;
+        this.nim = nim;
+        kelas = kls;
+    }
 }
