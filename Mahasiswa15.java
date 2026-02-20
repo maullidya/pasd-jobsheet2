5b98fa0d296b4e21f2b0f9b3ca9cfc2ad529f4ba
maullidya <zhramldy@gmail.com>
2026-02-20 09:06:25 +0700

    percobaan 1

---
+++ Mahasiswa15.java
@@ -0,0 +1,35 @@
+public class Mahasiswa15 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru){
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(){
+        if (0.0 >= ipk && 4.0 < ipk) {
+            return "ipk tidak valid";
+        } 
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
